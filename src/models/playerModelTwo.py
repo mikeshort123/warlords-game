c5f838793ff0b7d624aba3dabaef78968fd5c9dd
mikeshort123 <mike.short.nh@gmail.com>
2022-07-21 01:53:41 +0100

    Made a ball man and changed some render methods to allow him to draw

--- src/models/playerModelTwo.py
+++ src/models/playerModelTwo.py
@@ -9,34 +9,34 @@ class PlayerModelTwo:
 
     def __init__(self,model_data,modelTexture):
 
-        self.parts = {}
-
         self.scale = model_data["scale"]
 
-        for part, values in model_data["parts"].items():
+        self.parts = []
+        levels = {} # store level to order parts in order they should be drawn
 
-            imgCentre = Vector(values["centre"])
-            imgPos = Vector(values["pos"])
-            imgSize = Vector(values["size"])
+        for part in model_data["parts"]:
+
+            imgCentre = Vector(part["centre"])
+            imgPos = Vector(part["pos"])
+            imgSize = Vector(part["size"])
+
+            name = part["name"]
+            levels[name] = part["level"]
 
             centre = (imgPos - imgCentre + imgSize/2) / self.scale
-            offset = Vector(values["offset"]) / self.scale
+            offset = Vector(part["offset"]) / self.scale
 
             size = imgSize / self.scale
 
             img = pygame.Surface(imgSize.list(), pygame.SRCALPHA)
             img.blit(modelTexture, (0, 0), (imgPos.x, imgPos.y, imgSize.x, imgSize.y))
 
-            self.parts[part] = Model(img,size,centre,offset)
+            self.parts.append([name,Model(img,size,centre,offset)])
+
+        self.parts.sort(key=lambda p : levels[p[0]]) # sort parts by level
 
 
     def render(self,renderer,pos,cam,animator):
 
-        self.parts["body"].render(renderer,pos,cam,temp=animator.getPart("body"))
-        self.parts["left_foot"].render(renderer,pos,cam,temp=animator.getPart("left_foot"))
-        self.parts["right_foot"].render(renderer,pos,cam,temp=animator.getPart("right_foot"))
-
-        self.parts["left_hand"].render(renderer,pos,cam,temp=animator.getPart("left_hand"))
-        self.parts["right_hand"].render(renderer,pos,cam,temp=animator.getPart("right_hand"))
-
-        self.parts["head"].render(renderer,pos,cam,temp=animator.getPart("head"))
+        for name, part in self.parts:
+            part.render(renderer,pos,cam,temp=animator.getPart(name))
