ae34ab5d07a61ce087936677ba0244a886f5e058
mikeshort123 <mike.short.nh@gmail.com>
2022-08-03 19:31:50 +0100

    Scrollbar to select mods

--- src/uis/modScreen.py
+++ src/uis/modScreen.py
@@ -5,6 +5,7 @@ from src.states.state import State
 from src.mods.mod import Mod
 from src.utils.hitbox import Hitbox
 from src.uis.uiFrame import UIFrame
+from src.utils.vector import Vector
 
 class ModScreen(UIFrame):
 
@@ -20,11 +21,29 @@ class ModScreen(UIFrame):
         self.mod_list = Mod.mod_list
         self.modHitboxes = [Hitbox(56 + 150*i, 360, 144, 96) for i in range(len(self.mod_list))]
 
+        self.mod_bar_hitbox = Hitbox(56, 360, 528, 96)
+
+        self.leftscroll_hitbox = Hitbox(36, 360, 20, 96)
+        self.rightscroll_hitbox = Hitbox(584, 360, 20, 96)
+
+        self.mod_pixel_offset = 0
+
     def tick(self,handler):
 
         if handler.getKeyChanged("CLOSE_INVENTORY"):
             State.state.dropFrame()
             return
+
+        if handler.getKeyPressed("SELECT"):
+            pos = handler.getMousePos()
+
+            if self.leftscroll_hitbox.isInside(pos) and self.mod_pixel_offset > 0:
+                self.mod_pixel_offset -= 8
+                return
+
+            if self.rightscroll_hitbox.isInside(pos) and self.mod_pixel_offset < len(self.mod_list) * 150 - 528 - 6:
+                self.mod_pixel_offset += 8
+                return
 
         if handler.getKeyChanged("SELECT"):
             pos = handler.getMousePos()
@@ -32,15 +51,20 @@ class ModScreen(UIFrame):
             for i, hitbox in enumerate(self.slotHitboxes):
                 if hitbox.isInside(pos):
                     self.slots[i] = None
+                    return
 
-            for mod, hitbox in zip(self.mod_list, self.modHitboxes):
-                if hitbox.isInside(pos):
+            if self.mod_bar_hitbox.isInside(pos):
 
-                    if mod in self.slots: continue # dont put duplicate mods in
+                offset = Vector(self.mod_pixel_offset,0)
 
-                    index = self.getFirstEmptySlot()
-                    if index != None:
-                        self.slots[index] = mod
+                for mod, hitbox in zip(self.mod_list, self.modHitboxes):
+                    if hitbox.isInside(pos + offset):
+
+                        if mod in self.slots: continue # dont put duplicate mods in
+
+                        index = self.getFirstEmptySlot()
+                        if index != None:
+                            self.slots[index] = mod
 
 
     def getFirstEmptySlot(self):
@@ -56,6 +80,9 @@ class ModScreen(UIFrame):
 
         renderer.drawAlphaBackground((0,0,0),200)
 
+        pygame.draw.rect(renderer.display,(80,80,80),(36, 360, 20, 96))
+        pygame.draw.rect(renderer.display,(80,80,80),(584, 360, 20, 96))
+
         for slot, hitbox in zip(self.slots, self.slotHitboxes):
 
             if slot:
@@ -63,6 +90,10 @@ class ModScreen(UIFrame):
             else:
                 renderer.drawImage(self.blankslot,hitbox.ax,hitbox.ay)
 
-        for mod, hitbox in zip(self.mod_list, self.modHitboxes):
+        mod_bar = pygame.Surface((528,96), pygame.SRCALPHA)
 
-            renderer.drawImage(mod.img,hitbox.ax,hitbox.ay)
+        for i, (mod, hitbox) in enumerate(zip(self.mod_list, self.modHitboxes)):
+
+            mod_bar.blit(mod.img, (i*150 - self.mod_pixel_offset, 0))
+
+        renderer.drawImage(mod_bar, 56, 360)
